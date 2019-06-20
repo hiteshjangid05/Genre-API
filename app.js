ddff509d649d5752480a564e3ca39a52f5f66879
hiten2112 <hiten2112@3554567.no-reply.drupal.org>
2019-06-20 23:51:08 +0530

    Integretion with mongodb

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 const log = require('./routes/logger');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Vidly')
+.then(()=>{console.log("connected to database")})
+.catch(error=>{console.error("did not connect to datbase")})
+
 
 app.use(express.json());
 app.use('/api/genres', log);
 
 const port = process.env.PORT;
-app.listen(port,()=>{console.log(`you are listening to ${port}`)});
+app.listen(port,()=>{console.log(`you are listening to port  ${port}`)});
