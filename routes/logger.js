03b4e737dc758a862c5dfe8023c43b05f6c67fe2
Abhishek Singh <107251747+abhishekvinodsingh@users.noreply.github.com>
2022-10-18 19:03:04 +0530

    updated max length of Genre schema to 100.
    updated max length of Genre schema to 100.

--- routes/logger.js
+++ routes/logger.js
@@ -9,7 +9,7 @@ const GenreSchema = new mongoose.Schema({
         type : String,
         required :true,
         minlength : 5,
-        maxlength: 50
+        maxlength: 100
     }
 })
 const Genre = mongoose.model("Genre", GenreSchema);
